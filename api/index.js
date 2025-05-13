9971278a523ed9203f798a6342971ad0c3901b7f
gigi <dobrerobert441@gmail.com>
2025-05-13 15:55:54 +0300

    Alpha v0.9.4

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import User from './models/user.js';
-import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import cookieParser from 'cookie-parser';
 
+import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = 4000;
@@ -13,10 +14,11 @@ const JWT_SECRET = process.env.JWT_SECRET || 'default_jwt_secret';
 
 const app = express();
 app.use(cors({credentials: true,origin: 'http://localhost:3000'}));
-
 app.use(express.json());
+app.use(cookieParser())
 
-mongoose.connect(process.env.VITE_MONGO_KEY)
+
+mongoose.connect(process.env.VITE_MONGO_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
@@ -50,13 +52,36 @@ app.post('/login', async (req, res) => {
       return res.status(400).json({ error: 'Wrong credentials' });
     }
 
-    const token = jwt.sign({ id: userDoc._id, username: userDoc.username }, JWT_SECRET, { expiresIn: '1h' });
-    res.cookie('token', token).json('ok');
+    const token = jwt.sign({ id: userDoc._id, username: userDoc.username }, JWT_SECRET, {});
+    res.cookie('token', token, {
+    httpOnly: true,
+    sameSite: 'none',  
+    secure: false,
+    }).json({ message: 'Login successful' });
+
   } catch (err) {
     console.error('❌ Login error:', err);
     res.status(500).json({ error: 'Login failed' });
   }
 });
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  jwt.verify(token, JWT_SECRET, {}, (err, info) => {
+    if (err) {
+      console.error('❌ JWT verify error:', err);
+      return res.status(403).json({ error: 'Token verification failed' });
+    }
+
+    res.json(info);
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`🟩 Listening on port ${PORT}`);
--- blogAppClient/src/navBar.tsx
+++ blogAppClient/src/navBar.tsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavigationBar() {
-    const liStyle = "ml-3 hover:cursor-pointer w-[80px] hover:underline";
+    const liStyle = "ml-3 hover:cursor-pointer w-[100px] hover:underline";
+     const [username, setUsername] = useState(null)
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/profile',{
+            method: 'GET',
+            credentials: 'include',
+        }).then(res => {
+            res.json().then(userInfo => {
+                setUsername(userInfo.username)
+            })
+        })
+    }, [])
+
+    function logout(){
+        //
+    }
 
     return (
         <nav className="w-full max-w-screen-xl mx-auto h-10 mb-5 flex items-center">
             <ul className="flex h-full text-lg justify-between items-center w-full">
                 <Link to="/" className={`${liStyle} font-bold`}>My blog</Link>
                 <ul className="flex">
+            {username && (
+                <>
+                <Link className={liStyle} to="/create">New post</Link>
+                <a className={liStyle} onClick={(()=> logout())}>Logout</a>
+                </>
+            )}
+            {!username && (
+                <>
                     <Link to="/login" className={liStyle}>Login</Link>
                     <Link to="/register" className={liStyle}>Register</Link>
+                </>
+            )}
                 </ul>
             </ul>
         </nav>
